1d4e75acd1b70c53dd6dbd1531a2a51e473bce80
qandoni <yunev_i@mail.ru>
2026-03-01 16:31:53 +0400

    testing changes from Ubuntu

--- main.go
+++ main.go
@@ -18,24 +18,31 @@ func main() {
 		panic(err)
 	}
 
-	tasks, err := simple_sql.SelectRows(ctx, conn)
-	if err != nil {
-		panic(err)
-	}
-	for _, task := range tasks {
-		if task.ID == 4 {
-			task.Title = "Покормить кошку"
-			task.Description = "Отсыпать кошке 30 грамм корма"
-			task.Completed = true
-			now := time.Now()
-			task.CompletedAt = &now
+	simple_sql.InsertRow(ctx, conn, simple_sql.TaskModel{
+		Title:       "Сделать домашку",
+		Description: "Сделать домашку по русскому языку",
+		Completed:   false,
+		CreatedAt:   time.Now(),
+	})
 
-			if err := simple_sql.UpdateTask(ctx, conn, task); err != nil {
-				panic(err)
-			}
-			break
-		}
-	}
+	// tasks, err := simple_sql.SelectRows(ctx, conn)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// for _, task := range tasks {
+	// 	if task.ID == 4 {
+	// 		task.Title = "Покормить кошку"
+	// 		task.Description = "Отсыпать кошке 30 грамм корма"
+	// 		task.Completed = true
+	// 		now := time.Now()
+	// 		task.CompletedAt = &now
+
+	// 		if err := simple_sql.UpdateTask(ctx, conn, task); err != nil {
+	// 			panic(err)
+	// 		}
+	// 		break
+	// 	}
+	// }
 
 	fmt.Println("Успешно")
 }
