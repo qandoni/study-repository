6dfed95d228566d38033d5a4d39681ed9eaa3b52
qandoni <yunev_i@mail.ru>
2026-03-02 21:39:12 +0400

    docker

--- Makefile
+++ Makefile
@@ -1,7 +1,14 @@
 include .env
 export
+
+service-deploy:
+	docker compose up -d application
+
+service-undeploy:
+	docker compose down application
+
 service-run:
-	go run main.go
+	@go run main.go
 migrate-up:
 	migrate -path migrations -database ${CONN_STRING} up
 migrate-down:
---
+++ http_server/http_server.go
@@ -0,0 +1,20 @@
+package httpserver
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+)
+
+func StartHTTPServer() error {
+	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Обработка запроса на паттерне /ping")
+		w.Write([]byte("Hello from docker!\n"))
+	})
+	err := http.ListenAndServe(":5050", nil)
+	if errors.Is(err, http.ErrServerClosed) {
+		return nil
+	} else {
+		return err
+	}
+}
